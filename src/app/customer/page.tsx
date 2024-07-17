03542365b7d881d84d625a8417ed90349901978e
WorkingHardDev <vova.dev.work@gmail.com>
2024-07-17 10:15:51 +0300

    added mobile images

--- src/app/customer/page.tsx
+++ src/app/customer/page.tsx
@@ -2,7 +2,6 @@
 import { QASection } from '@/components/QASection/QASection';
 import { ExperienceDaisy } from '@/components/experienceDaisy/ExperienceDaisy';
 import { JoinTheDaisy } from '@/components/joinTheDaysi/JoinTheDaysi';
-import { NoScrollingAnimationBusiness } from '@/components/noAnimationBusiness/NoAnimationBusiness';
 import leaf from '../../assets/images/LeafBg.png';
 import { GrowthSectionCustomer } from '@/components/customerPage/GrowthCustomer';
 import { OurPartnersSection } from '@/components/ourPartnters/OurPartnersSection';
@@ -12,6 +11,7 @@ import axiosInstance, { baseURLImage } from '@/helpers/axiosConfig';
 import { useChangeLanguage } from '@/store/language';
 import LockerContainer from '@/components/lockerScrollingSection/LockerContainer/LockerContainer';
 import { Skeleton } from '@/components/ui/skeleton';
+import { NoScrollingAnimationBusiness } from '@/components/noAnimationBusiness/NoAnimationBusiness';
 
 const Customer = () => {
   const [heroCustomer, setHeroCustomer] = useState<any>();
@@ -32,7 +32,11 @@ const Customer = () => {
         const responseScrolling = await axiosInstance.get(
           `/home-customer-scrollings?populate=*&locale=${lang}`,
         );
-        setDataScroll(responseScrolling?.data?.data);
+        setDataScroll(
+          responseScrolling?.data?.data.sort(
+            (a: any, b: any) => a.attributes.sortId - b.attributes.sortId,
+          ),
+        );
         setGrowth(responseGrowth?.data?.data?.[0].attributes);
         setHeroCustomer(response?.data?.data?.[0].attributes);
       } catch (error) {
@@ -47,84 +51,58 @@ const Customer = () => {
         <div className="w-full bg-primary md:-mt-[100px]">
           <LockerContainer listInfo={dataScroll} />
           <div className="md:hidden">
-            <NoScrollingAnimationBusiness
-              subtitle={heroCustomer?.listPost[0].title}
-              title={heroCustomer?.listPost[0].subtitle}
-              desription={heroCustomer?.listPost[0].description}
-              list={heroCustomer?.listPost[0].listSub}
-              imageBg={leaf}
-              imageClassNameBg="absolute -bottom-[40px] left-0 w-[300px] h-[300px]"
-              image={
-                new URL(
-                  dataScroll?.[0].attributes.mainImage.data[0].attributes.formats.small.url,
-                  baseURLImage,
-                ).href
-              }
-              className="absolute w-full h-full bottom-0 left-10"
-              imageClassName=" absolute w-[220px] h-[320px] right-[50%] translate-x-1/2 bottom-0"
-              imageWidth={
-                dataScroll?.[0].attributes.mainImage.data[0].attributes.formats
-                  .small.width
-              }
-              imageHeight={
-                dataScroll?.[0].attributes.mainImage.data[0].attributes.formats
-                  .small.height
-              }
-            />
-            <div className="px-4 bg-primary">
-              <Separator className="bg-[#586968]" />
-            </div>
-            <NoScrollingAnimationBusiness
-              subtitle={heroCustomer?.listPost[1].title}
-              title={heroCustomer?.listPost[1].subtitle}
-              desription={heroCustomer?.listPost[1].description}
-              list={heroCustomer?.listPost[1].listSub}
-              imageBg={leaf}
-              imageClassNameBg="absolute -top-[70px] -left-[120px] w-[200px] h-[200px] rotate-180"
-              image={
-                new URL(
-                  dataScroll?.[1].attributes.mainImage.data[0].attributes.formats.small.url,
-                  baseURLImage,
-                ).href
-              }
-              imageWidth={
-                dataScroll?.[1].attributes.mainImage.data[0].attributes.formats
-                  .small.width
-              }
-              imageHeight={
-                dataScroll?.[1].attributes.mainImage.data[0].attributes.formats
-                  .small.height
-              }
-              className="absolute w-full h-full bottom-0 left-10 "
-              imageClassName=" absolute w-[230px] h-[300px] top-0  -translate-x-1/2 left-[50%]"
-            />
-            <div className="px-4 bg-primary">
-              <Separator className="bg-[#586968]  border-none" />
-            </div>
-            <NoScrollingAnimationBusiness
-              subtitle={heroCustomer?.listPost[2].title}
-              title={heroCustomer?.listPost[2].subtitle}
-              desription={heroCustomer?.listPost[2].description}
-              list={heroCustomer?.listPost[2].listSub}
-              imageWidth={
-                dataScroll?.[2].attributes.mainImage.data[0].attributes.formats
-                  .small.width
-              }
-              imageHeight={
-                dataScroll?.[2].attributes.mainImage.data[0].attributes.formats
-                  .small.height
-              }
-              imageBg={leaf}
-              imageClassNameBg="absolute -bottom-[50px] -right-[100px] w-[200px] h-[200px] rotate-[20deg]"
-              image={
-                new URL(
-                  dataScroll?.[2].attributes.mainImage.data[0].attributes.formats.small.url,
-                  baseURLImage,
-                ).href
-              }
-              className="absolute w-full h-full bottom-0 left-10 "
-              imageClassName=" absolute w-[260px] h-[320px] bottom-0 -translate-x-1/2 left-[50%]"
-            />
+            <ul>
+              {dataScroll &&
+                dataScroll.map(({ attributes }: any, index: number) => {
+                  return (
+                    <li key={index}>
+                      <NoScrollingAnimationBusiness
+                        styleBgMobileSecond={attributes.styleBgMobileSecond}
+                        sizeImage={attributes.styleImageMobile}
+                        styleFirstBgJSON={attributes.styleFirstBgJSON}
+                        styleSecondBgJSON={attributes.styleSecondBgJSON}
+                        imageBg={
+                          new URL(
+                            attributes.firstBg.data[0].attributes.url,
+                            baseURLImage,
+                          ).href
+                        }
+                        imageClassNameBg="absolute -bottom-[40px] left-0 w-[300px] h-[300px]"
+                        image={
+                          new URL(
+                            attributes.mainImage.data?.[0].attributes.formats.small.url,
+                            baseURLImage,
+                          ).href
+                        }
+                        className="absolute w-full h-full bottom-0 left-10 "
+                        // imageClassName=" absolute w-[300px]  top-[50%] -translate-y-1/2 right-0"
+                        imageBgTwo={
+                          new URL(
+                            attributes?.secondBg?.data?.[0]?.attributes?.url,
+                            baseURLImage,
+                          ).href
+                        }
+                        imageClassNameBgSecond="absolute -top-[70px] -right-[70px] w-[200px] h-[200px] rotate-180 scale-x-[-1]"
+                        desription={attributes.infoScroll.description}
+                        title={attributes.infoScroll.text}
+                        subtitle={attributes.infoScroll.title}
+                        list={attributes.infoScroll.listSub}
+                        imageWidth={
+                          dataScroll?.[1]?.attributes.mainImage.data?.[0]
+                            .attributes.formats.small.width
+                        }
+                        imageHeight={
+                          dataScroll?.[1]?.attributes.mainImage.data?.[0]
+                            .attributes.formats.small.height
+                        }
+                      />
+                      <div className="px-4 bg-primary">
+                        <Separator className="bg-[#586968]" />
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
           </div>
           <OurPartnersSection />
           <GrowthSectionCustomer
--- src/components/noAnimationBusiness/NoAnimationBusiness.tsx
+++ src/components/noAnimationBusiness/NoAnimationBusiness.tsx
@@ -3,6 +3,8 @@ import { CheckIcon } from '@/assets/icons/checkIcon/CheckIcon';
 import { Button } from '../ui/button';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
+import { PlayMarketButton } from '../buttonApp/PlayMarketButton';
+import { AppStoreButton } from '../buttonApp/AppStoreButton';
 
 export const NoScrollingAnimationBusiness = ({
   image,
@@ -19,11 +21,14 @@ export const NoScrollingAnimationBusiness = ({
   imageWidth,
   imageHeight,
   sizeImage,
+  styleFirstBgJSON,
+  styleSecondBgJSON,
+  styleBgMobileSecond,
 }: {
   image?: any;
   title: string;
   className?: string;
-  imageClassName: string;
+  imageClassName?: string;
   imageBg: any;
   imageBgTwo?: any;
   imageClassNameBg: string;
@@ -34,6 +39,9 @@ export const NoScrollingAnimationBusiness = ({
   imageWidth: any;
   imageHeight: any;
   sizeImage?: any;
+  styleFirstBgJSON: any;
+  styleSecondBgJSON: any;
+  styleBgMobileSecond?: any;
 }) => {
   const container = {
     hidden: { opacity: 1, scale: 0 },
@@ -58,12 +66,24 @@ export const NoScrollingAnimationBusiness = ({
   return (
     <div className="pt-16 bg-primary px-4 pb-[64px]">
       <div className="w-full relative bg-primaryBtn overflow-hidden rounded-[9px] h-[340px]">
-        <Image src={imageBg} alt="bg" className={imageClassNameBg} />
-        {imageBgTwo && (
+        {!imageBg.includes(undefined) && (
+          <Image
+            src={imageBg}
+            style={styleFirstBgJSON}
+            alt="bg"
+            className={imageClassNameBg}
+            width={1000}
+            height={1000}
+          />
+        )}
+        {!imageBgTwo.includes(undefined) && (
           <Image
             src={imageBgTwo}
+            style={styleBgMobileSecond}
             alt="bgtwo"
             className={imageClassNameBgSecond}
+            width={1000}
+            height={1000}
           />
         )}
         {!image.includes(undefined) && (
@@ -71,8 +91,8 @@ export const NoScrollingAnimationBusiness = ({
             src={image!}
             alt="photo"
             className={imageClassName}
-            width={imageWidth}
-            height={imageHeight}
+            width={imageWidth || 1000}
+            height={imageHeight || 1000}
             style={sizeImage}
           />
         )}
@@ -80,31 +100,44 @@ export const NoScrollingAnimationBusiness = ({
       <h5 className="text-[#F2DAD4] pt-8 text-base leading-6 uppercase">
         {subtitle}
       </h5>
-      <h2 className="pt-2 text-white leading-10 text-3xl">{title}</h2>
-      <p className="ltr:font-montserrat font-medium text-xl pt-3 leading-8 text-[#D5D9D9]">
+      <h2 className="pt-2 text-white leading-10 text-3xl capitalize">
+        {title}
+      </h2>
+      <p className="ltr:font-montserrat font-medium text-xl pt-3 leading-8 text-[#D5D9D9] capitalize">
         {desription}
       </p>
-      <motion.ul
-        variants={container}
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ once: true }}
-        className="mt-8"
-      >
-        {list?.map((items, index) => {
-          return (
-            <motion.li key={index} variants={item}>
-              <Button
-                variant="navigation"
-                className="w-full justify-start mt-4 font-semibold text-base text-[#D5D9D9]"
-              >
-                <CheckIcon className="ltr:mr-2 rtl:ml-2" />
-                {items}
-              </Button>
-            </motion.li>
-          );
-        })}
-      </motion.ul>
+      {list ? (
+        <motion.ul
+          variants={container}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          className="mt-8"
+        >
+          {list.map((items: any, index: number) => {
+            return (
+              <motion.li key={index} variants={item}>
+                <Button
+                  variant="navigation"
+                  className="w-full justify-start mt-4 font-semibold text-base text-[#D5D9D9]"
+                >
+                  <CheckIcon className="ltr:mr-2 rtl:ml-2" />
+                  {items}
+                </Button>
+              </motion.li>
+            );
+          })}
+        </motion.ul>
+      ) : (
+        <div className="grid mt-4  w-full">
+          <>
+            <div className="flex flex-col justify-between border-primaryBtn  ">
+              <PlayMarketButton className="mb-2 w-full" />
+              <AppStoreButton />
+            </div>
+          </>
+        </div>
+      )}
     </div>
   );
 };
