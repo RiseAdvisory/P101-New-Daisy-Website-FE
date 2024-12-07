46b9ea4c14d5d0789d41849f7f47af8caf30311b
Rahimi <110251351+Abdullrahim-Rahimi@users.noreply.github.com>
2024-12-07 15:50:14 +0300

    add our country codes

--- src/helpers/getCountryCodes.ts
+++ src/helpers/getCountryCodes.ts
@@ -1,24 +1,24 @@
 export const getData = async () => {
-    try {
-      const res = await fetch('https://app.trythedaisy.com/api/v1/countries', {
-        method: 'POST',
+  try {
+    const res = await fetch('https://app.trythedaisy.com/api/v1/countries', {
+      method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-                }
-      });
-  
-      if (!res.ok) {
-        throw new Error(`HTTP error! status: ${res.status}`);
-      }
-  
-      const responseData: any = await res.json();
-  
-      if (!responseData.status) throw new Error('No data found');
-  
-      return responseData.data.countries;
-    } catch (error) {
-      console.error('An error occurred while fetching the data:', error);
-      // You can return a default value or throw the error to handle it in the component
-      return null;
+      },
+    });
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! status: ${res.status}`);
     }
-  };
+
+    const responseData: any = await res.json();
+
+    if (!responseData.status) throw new Error('No data found');
+
+    return responseData.data.countries;
+  } catch (error) {
+    console.error('An error occurred while fetching the data:', error);
+    // You can return a default value or throw the error to handle it in the component
+    return null;
+  }
+};
