c83d9be281fc46f84227bf502938cec5657fe512
Rahimi <110251351+Abdullrahim-Rahimi@users.noreply.github.com>
2024-12-07 16:32:29 +0300

    add our country codes attempt 6

--- src/components/formBusiness/FormBusiness.tsx
+++ src/components/formBusiness/FormBusiness.tsx
@@ -300,13 +300,15 @@ export const ProfileForm = () => {
                         {array.map((item, i) => {
                           //console.log(item);
                           const country_code = item.country_code;
-                          // if (
-                          //   !item.image ||
-                          //   !country_code ||
-                          //   usedCountryCodes.has(country_code)
-                          // ) {
-                          //   return null;
-                          // }
+                          console.log(item.country_code);
+                          console.log(item.image);
+                          if (
+                            !item.image ||
+                            !country_code ||
+                            usedCountryCodes.has(country_code)
+                          ) {
+                            return null;
+                          }
 
                           usedCountryCodes.add(country_code);
 
