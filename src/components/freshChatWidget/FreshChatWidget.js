a9df0a4e97834c8522682f68b4c30385450cb953
Rahimi <110251351+Abdullrahim-Rahimi@users.noreply.github.com>
2024-12-21 16:37:40 +0300

    adding freshchat to website attempt 12

--- src/components/freshChatWidget/FreshChatWidget.js
+++ src/components/freshChatWidget/FreshChatWidget.js
@@ -53,7 +53,7 @@ function getScriptContent(lang) {
             function initialize(i,t){var e;i.getElementById(t)?
             initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,
             e.src="https://trythedaisy.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}
-            function initiateCall(){initialize(document,"Freshchat-js-sdk")}
+            // function initiateCall(){initialize(document,"Freshchat-js-sdk")}
             // window.addEventListener?window.addEventListener("load",initiateCall,!1):
             // window.attachEvent("load",initiateCall,!1);
             // Immediately initialize (no window.load)
@@ -70,7 +70,7 @@ function getScriptContent(lang) {
           function initialize(i,t){var e;i.getElementById(t)?
           initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,
           e.src="https://trythedaisy.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}
-          function initiateCall(){initialize(document,"Freshchat-js-sdk")}
+          // function initiateCall(){initialize(document,"Freshchat-js-sdk")}
           // window.addEventListener?window.addEventListener("load",initiateCall,!1):
           // window.attachEvent("load",initiateCall,!1);
           // Immediately initialize (no window.load)
