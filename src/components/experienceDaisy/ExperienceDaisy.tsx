e6510046302d0098797642eb5ecb323cd8936f7b
Abdulrahim Rahimi <110251351+Abdullrahim-Rahimi@users.noreply.github.com>
2025-06-05 09:35:48 +0300

    Update ExperienceDaisy.tsx

--- src/components/experienceDaisy/ExperienceDaisy.tsx
+++ src/components/experienceDaisy/ExperienceDaisy.tsx
@@ -86,7 +86,7 @@ export const ExperienceDaisy = () => {
           className="absolute -top-[70px] ltr:left-[60px] rtl:right-[60px] hidden md:block"
         />
 
-        <div className="md:w-1/2 md:px-20 mx-auto text-center ltr:md:text-left rtl:md:text-right">
+        <div className="ltr:md:ml-[560px] ltr:md:mr-[114px] rtl:md:mr-[560px] rtl:md:ml-[114px]">
           <h1 className="pt-5 text-white leading-10 text-3xl">
             {experienceDaisy?.title}
           </h1>
